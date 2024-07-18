f09747ea979f90cb2257280c35ed56e9ea2b891f
milbon-milbon <km.31515@gmail.com>
2024-07-18 21:02:02 +0900

    front_backend_ログ実装後_0718

---
+++ recipe-search-app/backend/src/app/lib/logger.py
@@ -0,0 +1,21 @@
+import os
+import logging
+from dotenv import load_dotenv
+
+# .env ファイルから環境変数を読み込む
+load_dotenv()
+
+# 環境変数からログレベルを取得（デフォルトは 'INFO'）
+log_level = os.getenv('LOG_LEVEL', 'INFO').upper()
+
+# ログ設定
+logging.basicConfig(
+    level=log_level,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    handlers=[
+        logging.StreamHandler(),
+        logging.FileHandler('logfile.log')
+    ]
+)
+logger = logging.getLogger('app')
+logger.setLevel(log_level)
